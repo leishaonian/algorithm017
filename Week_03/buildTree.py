9c6b3af85b100b005dc42ab4ef8e2fd871e62cd6
雷 <2839811677@qq.com>
2020-10-25 01:23:46 +0800

    提交作业

---
+++ Week_03/buildTree.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if not preorder or not inorder:
+            return 
+        root = TreeNode(preorder[0])
+        idx = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:idx + 1], inorder[:idx])
+        root.right = self.buildTree(preorder[idx + 1:], inorder[idx + 1:])
+        return root
